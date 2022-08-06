184158b3986a179dc22c80749d3bfd26302ea065
H_A _Opu <wwwhasib4@gmail.com>
2022-08-06 17:33:18 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CustomerReviews from "./Components/CustomerReviews/CustomerReviews";
 import DayServices from "./Components/DayServices/DayServices";
 import DiscountPart from "./Components/DiscountPart/DiscountPart";
 import ElectricianServices from "./Components/ElectricianServices/ElectricianServices";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import PlumberServices from "./Components/PlumberServices/PlumberServices";
 import ReferSection from "./Components/ReferSection/ReferSection";
@@ -33,6 +34,7 @@ function App() {
       <CustomerReviews />
       <ReferSection />
       <StampSection />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,93 @@
+import React from "react";
+import playStore from "../../images/google-play.png";
+import Instagram from "../../images/icon/Instagram.png";
+import point from "../../images/icon/point.png";
+import linkedin from "../../images/icon/Linkedin.png";
+import facebook from "../../images/icon/facebook.png";
+import youtube from "../../images/icon/youtube.png";
+import twitter from "../../images/icon/twitter.png";
+
+const Footer = () => {
+  return (
+    <footer class="footer footer-center text-start p-10 bg-[#343434] text-white">
+      <div className="flex gap-10 font-semibold">
+        <p>RELATED SERVICES</p>
+        <div className="px-6">
+          <span className="border-r-2 px-2">Electrician</span>
+          <span className="border-r-2 px-2">Plumber</span>
+          <span className="border-r-2 px-2">Carpenter</span>
+          <span className="border-r-2 px-2">Packers & Movers</span>
+          <span className="border-r-2 px-2">Salon for Women</span>
+          <span className="border-r-2 px-2">Salon for Men</span>
+          <span className="border-r-2 px-2">Electronic Appliances</span>
+          <span className="border-r-2 px-2">CCTV Installation & Repair</span>
+          <span className="border-r-2 px-2">Re-Repair & Service</span>
+          <span className="border-r-2 px-2">House Painter</span>
+          <span className="border-r-2 px-2">Pest Control</span>
+          <span className="border-r-2 px-2">Kitchen Cleaning</span>
+          <span className="border-r-2 px-2">Bathroom Cleaning</span>
+          <span className="border-r-2 px-2">Sofa Cleaning</span>
+          <span className="border-r-2 px-2">Carpet cleaning</span>
+          <span className="pl-2">Home Deep Cleaning</span>
+        </div>
+      </div>
+      <div className="flex gap-10 font-semibold">
+        <p>TOP LOCATIONS</p>
+        <div className="px-6">
+          <span className="border-r-2 px-2">Delhi</span>
+          <span className="border-r-2 px-2">Gurgaon</span>
+          <span className="border-r-2 px-2">Noida</span>
+          <span className="border-r-2 px-2">Ghaziabad</span>
+          <span className="border-r-2 px-2">Faridhabad</span>
+          <span className="pl-2">Greater Noida</span>
+        </div>
+      </div>
+      <div className="flex gap-10 text-lg font-semibold">
+        <span>About Us </span>
+        <span>Blog</span>
+        <span>Near me</span>
+        <span>Careers</span>
+        <span>Terms & Conditions</span>
+        <span>Privacy Policy</span>
+        <span>Offers & Gift Cards</span>
+        <span>Contact Us</span>
+      </div>
+      <div className="flex gap-12">
+        <div className="">
+          <h1 className="text-[#2AEAEE] font-semibold">Contact Us</h1>
+          <p>Phone: +919990001089</p>
+          <p>Email: support@oyebusy.com</p>
+        </div>
+        <div className="">
+          <h1 className="text-[#2AEAEE] font-semibold">Download Oyebusy App</h1>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.oyebusy.oyebusy_customer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="hover:scale-110 duration-300 ease-in-out"
+              src={playStore}
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div>
+        <div class="grid grid-flow-col gap-7">
+          <img src={twitter} alt="" />
+          <img src={facebook} alt="" />
+          <img src={youtube} alt="" />
+          <img src={linkedin} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={point} alt="" />
+        </div>
+        <span className="py-10 tracking-wider">
+          © 2022 OyeBusy Technologies Pvt. Ltd.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
