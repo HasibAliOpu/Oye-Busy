80a9e1051311d710fe71cd2b449e756228700e10
H_A _Opu <wwwhasib4@gmail.com>
2022-08-06 19:14:48 +0600

    carpenter and plumber services section responsive done

--- src/Components/CarpenterServices/CarpenterServices.js
+++ src/Components/CarpenterServices/CarpenterServices.js
@@ -7,52 +7,65 @@ import Fittings from "../../images/Fitting___minor_installations.png";
 import furniture from "../../images/Furniture_assembly.png";
 const CarpenterServices = () => {
   return (
-    <div className="flex pl-8 mb-10 mt-20">
+    <div className="lg:flex lg:pl-8 mb-10 mt-20">
       <div className="text-center mt-14 mb-14">
         <h1 className="text-3xl font-semibold text-[#025B5D]">
-          carpenter Services
+          Carpenter Services
         </h1>
         <p className="text-xl font-semibold pt-2">Premium Home Services</p>
       </div>
-      <div className="grid grid-cols-6 gap-5 p-20">
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-5 p-5 lg:p-20">
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="-mt-20" src={bed} alt="" />
-          <p className="font-semibold ">Bed</p>
-          <p className="text-sm text-[#757575]">Repairing Support, Legs</p>
-        </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={curtain} alt="" />
-          <p className="font-semibold ">Curtain & Blinds</p>
-          <p className="text-sm text-[#757575]">
-            Motorized, Non-Motorized <br />
-            Blinds & Curtain Rod
+          <img className="-mt-10 lg:-mt-20" src={bed} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">Bed</p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Repairing & Installation
           </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto w-1/2 -mt-20" src={door} alt="" />
-          <p className="font-semibold ">Door</p>
-          <p className="text-sm text-[#757575]">
-            Repairing, Accessories,
-            <br />
-            Installation
+          <img className="mx-auto -mt-10 lg:-mt-20" src={curtain} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Curtain & Blinds
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Shower, Faucets & Holder <br /> Installation & Repair
           </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={cupboard} alt="" />
-          <p className="font-semibold ">Drawer & Cupboard</p>
-          <p className="text-sm text-[#757575]">Channel, Hinge Repair</p>
+          <img className="mx-auto -mt-10 lg:-mt-20" src={door} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">Door</p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Toilet pot, Sink ,<br />
+            Bathroom, Balcony pipe
+          </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={Fittings} alt="" />
-          <p className="font-semibold ">Fittings & Installations</p>
-          <p className="text-sm text-[#757575]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={cupboard} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Drawer & Cupboard
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Painting, Clock, &<br />
+            Photoframe
+          </p>
+        </div>
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={Fittings} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Fittings & Installations
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
             Holder, Hanger, Rod <br /> Installation
           </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="-mt-20" src={furniture} alt="" />
-          <p className="font-semibold ">Furniture Assembly</p>
-          <p className="text-sm text-[#757575]">Bed, Table, Rock & Cabinet</p>
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="-mt-10 lg:-mt-20" src={furniture} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Furniture Assembly
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Bed, Table, Rock & Cabinet
+          </p>
         </div>
       </div>
     </div>
--- src/Components/PlumberServices/PlumberServices.js
+++ src/Components/PlumberServices/PlumberServices.js
@@ -7,53 +7,63 @@ import minor from "../../images/Minor_installations.png";
 import tap from "../../images/Tap.png";
 const PlumberServices = () => {
   return (
-    <div className="flex pl-8 mb-10 mt-20">
+    <div className="lg:flex lg:pl-8 mb-10 mt-20">
       <div className="text-center mt-14 mb-14">
         <h1 className="text-3xl font-semibold text-[#025B5D]">
           Plumber Services
         </h1>
         <p className="text-xl font-semibold pt-2">Premium Home Services</p>
       </div>
-      <div className="grid grid-cols-6 gap-5 p-20">
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-5 p-5 lg:p-20">
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="-mt-20" src={basin} alt="" />
-          <p className="font-semibold ">Wash Basin & Sink</p>
-          <p className="text-sm text-[#757575]">Repairing & Installation</p>
+          <img className="-mt-10 lg:-mt-20" src={basin} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Wash Basin & Sink
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Repairing & Installation
+          </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={bathFittings} alt="" />
-          <p className="font-semibold ">Bath Fittings</p>
-          <p className="text-sm text-[#757575]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={bathFittings} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">Bath Fittings</p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
             Shower, Faucets & Holder <br /> Installation & Repair
           </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={blockage} alt="" />
-          <p className="font-semibold ">Blockage</p>
-          <p className="text-sm text-[#757575]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={blockage} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">Blockage</p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
             Toilet pot, Sink ,<br />
             Bathroom, Balcony pipe
           </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={drill} alt="" />
-          <p className="font-semibold ">Drill & Hole</p>
-          <p className="text-sm text-[#757575]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={drill} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">Drill & Hole</p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
             Painting, Clock, &<br />
             Photoframe
           </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="mx-auto -mt-20" src={minor} alt="" />
-          <p className="font-semibold ">Minor Installations</p>
-          <p className="text-sm text-[#757575]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="mx-auto -mt-10 lg:-mt-20" src={minor} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Minor Installations
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
             Holder, Hanger, Rod <br /> Installation
           </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
-          <img className="-mt-20" src={tap} alt="" />
-          <p className="font-semibold ">Mixer, Tap & Nozzle</p>
-          <p className="text-sm text-[#757575]">Bed, Table, Rock & Cabinet</p>
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+          <img className="-mt-10 lg:-mt-20" src={tap} alt="" />
+          <p className="text-sm lg:text-base font-semibold ">
+            Mixer, Top & Nozzle
+          </p>
+          <p className="text-2xs lg:text-sm text-[#757575]">
+            Bed, Table, Rock & Cabinet
+          </p>
         </div>
       </div>
     </div>
