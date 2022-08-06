695e2709fb66156c2ab424414cb0e3eb0f8989a7
H_A _Opu <wwwhasib4@gmail.com>
2022-08-06 18:32:18 +0600

    responsive safe service banner

--- src/Components/SafeServices/SafeServices.js
+++ src/Components/SafeServices/SafeServices.js
@@ -2,12 +2,12 @@ import React from "react";
 import deliveryMan from "../../images/delivery-man-red-uniform.png";
 const SafeServices = () => {
   return (
-    <div className="grid grid-cols-2 my-14 mx-40 bg-[#ECECEE] rounded-xl shadow-xl">
-      <div className="">
-        <h1 className="text-5xl text-[#025B5D] pt-14 text-center font-semibold">
+    <div className="grid grid-cols-2 my-14 lg:mx-40 mx-5 bg-[#ECECEE] rounded-xl shadow-xl">
+      <div>
+        <h1 className="text-sm lg:text-5xl text-[#025B5D] pt2 lg:pt-14 text-center font-semibold">
           100% Safe Services
         </h1>
-        <span className="flex gap-24 text-2xl pl-24 pt-8">
+        <span className="flex gap-8 lg:gap-24 text-2xs lg:text-2xl pl-8 lg:pl-24 lg:pt-8">
           <ul className="list-disc">
             <li>Masks</li>
             <li>Gloves</li>
