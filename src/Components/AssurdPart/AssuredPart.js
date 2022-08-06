e7605c272356fab9c1f3d1499beb0ac6b20305fb
H_A _Opu <wwwhasib4@gmail.com>
2022-08-06 18:37:31 +0600

    making responsive assurd peace banner

--- src/Components/AssurdPart/AssuredPart.js
+++ src/Components/AssurdPart/AssuredPart.js
@@ -5,16 +5,18 @@ import wallet from "../../images/icon/wallet.png";
 
 const AssuredPart = () => {
   return (
-    <div className="bg-[#ECECEE] my-8 p-10">
-      <h1 className="text-center pt-8 pb-14 text-6xl font-bold">
+    <div className="bg-[#ECECEE] my-8 p-6 lg:p-10">
+      <h1 className="text-center pt-4 lg:pt-8 pb-7 lg:pb-14 text-3xl lg:text-6xl font-bold">
         Assured Peace of Mind
       </h1>
-      <div className="flex justify-center gap-10">
+      <div className="lg:flex justify-center gap-10">
         <div className="flex items-center gap-3">
           <img src={setting} alt="" />
           <span>
-            <h2 className="text-2xl font-semibold">High Quality Work</h2>
-            <p>
+            <h2 className="text-xl lg:text-2xl font-semibold">
+              High Quality Work
+            </h2>
+            <p className="text-xs lg:text-base">
               Only authorized Service experts and <br />
               genuine spare parts and equipments
             </p>
@@ -23,8 +25,8 @@ const AssuredPart = () => {
         <div className="flex items-center gap-3">
           <img src={human} alt="" />
           <span>
-            <h2 className="text-2xl font-semibold">Hassle Free</h2>
-            <p>
+            <h2 className="text-xl lg:text-2xl font-semibold">Hassle Free</h2>
+            <p className="text-xs lg:text-base">
               Sit back and relax. <br />
               We do all the work
             </p>
@@ -33,8 +35,10 @@ const AssuredPart = () => {
         <div className="flex items-center gap-3">
           <img src={wallet} alt="" />
           <span>
-            <h2 className="text-2xl font-semibold">Totally Cashless</h2>
-            <p>
+            <h2 className="text-xl lg:text-2xl font-semibold">
+              Totally Cashless
+            </h2>
+            <p className="text-xs lg:text-base">
               Pay online for Safe & Secure payment. <br /> many benefits and
               offers available with online payment
             </p>
