bc059516443a0532f1a424732c3c1bcb58b2e82c
H_A _Opu <wwwhasib4@gmail.com>
2022-08-06 18:41:35 +0600

    making responsive home appliances services section

--- src/Components/AppliancesServices/AppliancesServices.js
+++ src/Components/AppliancesServices/AppliancesServices.js
@@ -9,41 +9,43 @@ const AppliancesServices = () => {
   return (
     <div>
       <div className="text-center mt-14 mb-14">
-        <h1 className="text-5xl font-semibold text-[#025B5D]">
+        <h1 className="text-2xl lg:text-5xl font-semibold text-[#025B5D]">
           Home Appliances Services
         </h1>
-        <p className="text-xl font-semibold pt-2">Premium Home Services</p>
+        <p className="lg:text-xl font-semibold pt-2">Premium Home Services</p>
       </div>
-      <div className="grid grid-cols-6 gap-5 p-20">
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-5 p-5 lg:p-20">
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="-mt-20" src={washingMachine} alt="" />
-          <p className="font-semibold ">
+          <p className="text-xs lg:text-base font-semibold ">
             Washing Machine <br />
             Repair
           </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="mx-auto -mt-20" src={Freeze} alt="" />
-          <p className="font-semibold ">
+          <p className="text-xs lg:text-base font-semibold ">
             Refrigerator <br />
             Repair / Service
           </p>
         </div>
         <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="mx-auto -mt-20" src={oven} alt="" />
-          <p className="font-semibold ">Microwave Repair</p>
+          <p className="text-xs lg:text-base font-semibold ">
+            Microwave Repair
+          </p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="mx-auto -mt-20" src={RO} alt="" />
-          <p className="font-semibold ">RO Service</p>
+          <p className="text-xs lg:text-base font-semibold ">RO Service</p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="mx-auto -mt-20" src={ledTV} alt="" />
-          <p className="font-semibold ">LED TV Repair</p>
+          <p className="text-xs lg:text-base font-semibold">LED TV Repair</p>
         </div>
-        <div className="p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
+        <div className="mt-16 lg:mt-0 p-3 text-center rounded-2xl shadow-xl hover:scale-110 ease-in-out duration-500 bg-gradient-to-r hover:from-[#FFA500] hover:to-[#FFECCA]">
           <img className="-mt-20" src={AC} alt="" />
-          <p className="font-semibold ">AC Services</p>
+          <p className="text-xs lg:text-base font-semibold ">AC Services</p>
         </div>
       </div>
     </div>
